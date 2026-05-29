6e2822010a4b5c61d06b552af7f767dc8fa9908d
nook0110 <nik.blokhtin@gmail.com>
2026-05-29 20:56:16 +0300

    fix

--- load-tests/load_test.js
+++ load-tests/load_test.js
@@ -67,7 +67,7 @@ export default function () {
       try {
         const body = JSON.parse(r.body);
         return body.status !== undefined;
-      } catch {
+      } catch (e) {
         return false;
       }
     },
